a10fbb01243b033ed900bb58ce927c1ecfe92fcd
ghigbie <gehigbie@gmail.com>
2021-02-14 19:29:40 -0500

    Initial commit

---
+++ BuildNetflix2/BuildNetflix2App.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflix2App.swift
+//  BuildNetflix2
+//
+//  Created by George Higbie on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflix2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
