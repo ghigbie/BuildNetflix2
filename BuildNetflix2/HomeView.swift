0915897c462af22a81ccb9036ef81b5377d3e421
ghigbie <gehigbie@gmail.com>
2021-02-14 19:54:08 -0500

    Add kingfisher dependency

---
+++ BuildNetflix2/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  BuildNetflix2
+//
+//  Created by George Higbie on 2/14/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Netflix")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
